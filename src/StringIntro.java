6012a3e0d5c4d67f9969438c72881d793f2bdae2
dlin <david.lin@smes.org>
2022-08-24 12:38:07 -0700

    Initial commit

---
+++ src/StringIntro.java
@@ -0,0 +1,28 @@
+//a java class is a program file
+//the class name MUST start with a capital letter
+//and have no spaces
+
+//the class name must match the file name exactly
+
+//all code we write will go in the curly braces
+//for the class
+public class StringIntro {
+
+    //for now, all code will go in the main()
+    //method
+    public static void main(String [] args) {
+        System.out.print("Hi");
+        //"Hi" is a string
+        //Strings have text and numbers and symbols
+        //that can be used as data or printed
+        //Strings don't get interpreted as code
+
+        //without quotes, java thinks Hi is a variable
+        //and an error occurs
+//        System.out.print(Hi);
+
+
+
+    }
+
+}
